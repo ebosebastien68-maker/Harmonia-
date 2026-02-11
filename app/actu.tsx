df057574f1384bda144928861852b69058706861
ebosebastien68-maker <ebosebastien68@gmail.com>
2026-02-11 10:16:24 +0100

    Update actu.tsx

--- app/actu.tsx
+++ app/actu.tsx
@@ -610,8 +610,8 @@ export default function ActuScreen() {
           {posts.length === 0 ? (
             <View style={styles.emptyPosts}>
               <Ionicons name="newspaper-outline" size={60} color="#CCC" />
-              <Text style={styles.emptyText}>Aucune publication</Text>
-              <Text style={styles.emptySubtext}>Créez votre premier post !</Text>
+              <Text style={styles.emptyText}>Connexion en cours ...</Text>
+              <Text style={styles.emptySubtext}>Veuillez patienter</Text>
             </View>
           ) : (
             posts.map((post) => <PostCard key={post.id} post={post} />)
